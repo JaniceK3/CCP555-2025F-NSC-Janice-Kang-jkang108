d988d3a3e94e5085c96fbc1cc04e8aed021866d5
Janice Jiyeon Kang <kanggj0330@gmail.com>
2025-09-23 22:31:41 -0400

    Add basic auth for tests; wire routes, health and fragments tests

---
+++ fragments/tests/unit/health.test.js
@@ -0,0 +1,25 @@
+// fragments/tests/unit/health.test.js
+const request = require('supertest');
+const app = require('../../src/app');
+const { version, author } = require('../../package.json');
+
+describe('/ health check', () => {
+  test('200 OK', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+  });
+  test('Cache-Control: no-cache', async () => {
+    const res = await request(app).get('/');
+    expect(res.headers['cache-control']).toEqual('no-cache');
+  });
+  test('status: ok', async () => {
+    const res = await request(app).get('/');
+    expect(res.body.status).toBe('ok');
+  });
+  test('author/version/githubUrl present', async () => {
+    const res = await request(app).get('/');
+    expect(res.body.author).toEqual(author);
+    expect(res.body.version).toEqual(version);
+    expect(res.body.githubUrl.startsWith('https://github.com/')).toBe(true);
+  });
+});

aa954d568e0381c3acfeadcd09f5e5aef69b7677
Janice Jiyeon Kang <kanggj0330@gmail.com>
2025-10-29 00:26:20 -0400

    file updates for lab6

--- fragments/src/routes/api/get.js
+++ fragments/src/routes/api/get.js
@@ -1,14 +1,18 @@
 // src/routes/api/get.js
-
+const express = require('express');
 const Fragment = require('../../model/fragment');
 const { createSuccessResponse } = require('../../response');
+const router = express.Router();
 
-module.exports = async (req, res, next) => {
+// GET /v1/fragments -> IDs by default; ?expand=1|true -> metadata objects
+router.get('/fragments', async (req, res, next) => {
   try {
-    const expand = req.query.expand === '1';
-    const fragments = await Fragment.byUser(req.user.ownerId, expand);
+    const expand = req.query.expand === '1' || req.query.expand === 'true';
+    const fragments = await Fragment.byUser(req.user, expand);
     res.status(200).json(createSuccessResponse({ fragments }));
   } catch (err) {
     next(err);
   }
-};
+});
+
+module.exports = router;
